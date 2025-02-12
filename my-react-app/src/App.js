302593131efc41f15e35b91dbadb502b4c226686
Ejiro92 <eagbaisi@gmail.com>
2025-02-12 14:24:58 +0000

    Initial commit - React frontend

---
+++ my-react-app/src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://flask-receipt-api.onrender.com/your-endpoint")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>React + Flask API</h1>
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>}
+    </div>
+  );
+}
+
+export default App;
